840db278dbf4921aa770b01689043189b86440f2
alikaratana <alikaratana@gmail.com>
2016-11-27 21:41:13 +0300

    Addes some advaced search operators

--- src/main_screen.java
+++ src/main_screen.java
@@ -1,3 +1,5 @@
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
 import javax.swing.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -5,6 +7,8 @@ import java.awt.Desktop;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by ali on 15.11.2016.
@@ -14,18 +18,28 @@ public class main_screen {
 
     private JPanel panel1;
     private JTabbedPane tabbedPane1;
-    private JTextField textField1;
+    private JTextField all_words;
     private JButton searchButton;
     private JPanel ags;
     private JPanel ghdb;
     private JPanel zday;
+    private JTextField exact_word;
+    private JTextField any_words;
+    private JTextField non_words;
+    private JTextField textField5;
+    private JTextField textField6;
+    public SearchOperators searchOperators;
+    public String url="http://www.google.com/";
+
 
     public main_screen() {
+
+        searchOperators=new SearchOperators();
         searchButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                String url = create_link();
-
+                url+="search?";
+                url=create_link(url);
                 if (Desktop.isDesktopSupported()) {
                     // Windows
                     try {
@@ -57,10 +71,46 @@ public class main_screen {
 
     }
 
-    public String create_link()
+    public String create_link(String url)
     {
-        String url= "https://www.google.com.tr/#q="+textField1.getText();
-        return  url;
+        if(!all_words.getText().equals(""))
+        {
+            String[] words=all_words.getText().split(",");
+            for(int i=0;i<words.length;i++)
+            {
+                words[i]=words[i].replaceAll(" ","+");
+            }
+            url=searchOperators.all_the_words(url,words)+"&";
+        }
+
+        if(!exact_word.getText().equals(""))
+        {
+
+        }
+
+        if(!any_words.getText().equals(""))
+        {
+
+            String[] words=any_words.getText().split(",");
+            for(int i=0;i<words.length;i++)
+            {
+                words[i]=words[i].replaceAll(" ","+");
+            }
+            url=searchOperators.any_of_words(url,words)+"&";
+        }
+
+        if(!non_words.getText().equals(""))
+        {
+            String[] words=non_words.getText().split(",");
+            for(int i=0;i<words.length;i++)
+            {
+                words[i]=words[i].replaceAll(" ","+");
+            }
+            url=searchOperators.non_of_words(url,words)+"&";
+
+        }
+
+        return url;
     }
 
 
